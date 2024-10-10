d10d9e6acc9c180bba735af7f5e056efe15ff3ff
Mehmet ERDOĞAN <mehmet.erdogan@aktekbilisim.com>
2024-10-11 01:34:48 +0300

    Refactor FastAPI app for indexing and searching
    This commit refactors the FastAPI app in app.py to improve the indexing and searching functionality. The changes include:
    - Renaming the index_app variable to app for clarity
    - Adding a new endpoint for indexing documents
    - Modifying the IndexRequest model to include additional parameters
    - Updating the RAG.index function to use the new parameters
    - Removing the use_faiss parameter from the RAG.index function
    - Adding a new endpoint for searching queries
    - Modifying the QueryRequest model to remove the query separator character
    - Updating the RAG.search function to handle multiple queries
    
    These changes enhance the overall performance and flexibility of the app for indexing and searching documents.

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ from ragatouille import RAGPretrainedModel
 # RAG modelini yükle
 RAG = RAGPretrainedModel.from_pretrained("jinaai/jina-colbert-v2")
 
-# Döküman indekslemek için kullanılacak FastAPI uygulaması
-index_app = FastAPI()
+# FastAPI uygulaması
+app = FastAPI()
 
+# İndeksleme isteği modeli
 class IndexRequest(BaseModel):
     full_document: str
     document_id: str
     metadata: dict
     index_name: str
-    max_document_length: int = 4096  # Varsayılan değeri burada belirleyebilirsiniz
+    max_document_length: int = 4096
     split_documents: bool
 
-@index_app.post("/index")
+# İndeksleme endpoint'i
+@app.post("/index")
 async def index_document(index_request: IndexRequest):
     RAG.index(
         collection=[index_request.full_document],
@@ -25,23 +27,16 @@ async def index_document(index_request: IndexRequest):
         index_name=index_request.index_name,
         max_document_length=index_request.max_document_length,
         split_documents=index_request.split_documents,
-        # use_faiss=True
     )
     return {"status": "indexed", "document_id": index_request.document_id}
 
-# Sorgu yapmak için kullanılacak FastAPI uygulaması
-search_app = FastAPI()
-
+# Sorgu isteği modeli
 class QueryRequest(BaseModel):
     query: str
 
-@search_app.post("/search")
+# Sorgu endpoint'i
+@app.post("/search")
 async def search_rag(query_request: QueryRequest):
-    # Sorguları ayırmak için özel bir karakter kullanıyoruz (örneğin '|')
     queries = query_request.query.split('|')
-    
-    # Çoklu sorguları RAG.search fonksiyonuna gönderiyoruz
     result = RAG.search(query=queries)
-    
-    # Sonuçları döndürüyoruz
     return {"result": result}
